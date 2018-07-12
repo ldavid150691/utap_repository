2bb4fff57b740a3cd9312075f7b0f333d0d7a334
“ldavid150691” <jefferdavid77@gmail.com>
2018-07-11 20:40:53 -1100

    #

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "users" => "users#index" #Alias de naveacion de pagina web(controlador/accion (Metodo)) localhost:3000/users
+  post "users/login"# Peticiones POST submit matricular controlador/accion
+
 end
