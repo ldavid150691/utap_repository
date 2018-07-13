ff5e4562607a9ffa40c62a6475596049e85361a4
“ldavid150691” <jefferdavid77@gmail.com>
2018-07-12 21:27:08 -1100

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
     def login
         
          if User.exists?(:login=>params[:txtUser],:password=>params[:txtPassword])
-            @mensaje= "Usuario correcto"
-            @tipo="succes"
-	        render 'index'
+           # @mensaje= "Usuario correcto"
+           # @tipo="success"
+	        render 'home' ,layout:"home"  #redireccionar con layout incluido
         else
             @mensaje= "Usuario incorrecto"
             @tipo="error"
@@ -21,7 +21,32 @@ class UsersController < ApplicationController
         end
     end
 
+    def getProfiles
+        @perfiles=Profile.all
+        render "register"
+    end    
 
+    def createUser
+        mensaje=""
+        if !params.presence?(params[:txtLogin]) mensaje+= "Ingrese login" 
+        if params[:txtPassword].eql?("")  mensaje+= "Ingrese Password" 
+        if params[:txtPerfil].eql?("")  mensaje+= "Ingrese Password" 
+        if !params[:txtPassword].eql?(params[:lblPasswordConfirm])  mensaje+= "Password no coinciden"     
 
+            user_search=User.order_by("id").last
+            id= user_search.id+1
+
+            user=User.new
+            user.login=paramas[:txtLogin]
+            user.password=paramas[:txtPassword]
+            #user.perfil_id=paramas[:ddlPerfiles]
+            user.perfil_id=id
+
+            if user.save?    #se ejecuto el guardar?
+
+            else
+
+            end          
+    end
 
 end
